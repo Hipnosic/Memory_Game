2d83fb9e51f0150b4e98e68c627a3f31a78fbb1c
ledei <holybuzz@live.se>
2022-11-25 11:42:41 +0100

    update

--- Memory_Game-av-Daniel-Niklas-Hampus/js/app.js
+++ Memory_Game-av-Daniel-Niklas-Hampus/js/app.js
@@ -1,40 +1,46 @@
 let cardContainer = document.querySelector('.card-container')
 let startBtn = document.querySelector('.start-btn')
 let memoryCards = document.querySelectorAll('.card-container')
-let img = document.querySelector('.card-img')
-let rotate = document.querySelector('.card')
 let matchedItem = []
-
-function gameLogic(theme) {
-  matchedItem.push(theme.alt)
-  console.log(matchedItem)
-  if (matchedItem[0] == matchedItem[1]) {
-    console.log('hej')
-  } else if (matchedItem[1]) {
-    matchedItem = []
-    console.log('nope')
-  }
-}
 
 function createCard(theme) {
   let card = document.createElement('figure')
   card.innerHTML = `<img class ="card-img" src="${theme.name}" alt="${theme.alt}" width = "100">`
   card.className = 'card'
   card.addEventListener('click', () => {
-    handleClick(card)
-    gameLogic(theme)
+    gameLogic(card, theme)
   })
 
   return card
 }
 
-function handleClick(cards) {
-  cards.classList.add('active')
+function flipback() {
+  let rotate = document.querySelectorAll('.flip-front')
+  rotate[0].classList.remove('flip-front')
+  rotate[0].classList.add('flip-back')
+  rotate[1].classList.remove('flip-front')
+  rotate[1].classList.add('flip-back')
+}
+
+function gameLogic(card, theme) {
+  card.classList.add('flip-front')
+  card.classList.remove('flip-back')
+
+  matchedItem.push(theme.alt)
+  console.log(matchedItem)
+  if (matchedItem[0] == matchedItem[1]) {
+    console.log('hej')
+    matchedItem = []
+  } else if (matchedItem[1]) {
+    matchedItem = []
+    console.log('nope')
+    setTimeout(flipback, 1000)
+  }
 }
 
 function addCardImg(container) {
-  // var för funkar inte theme.length iställer för 24?
-  for (let i = 0; i < 24; i++) {
+  let totalAmount = theme.length
+  for (let i = 0; i < totalAmount; i++) {
     let random = Math.floor(Math.random() * theme.length)
     let cards = createCard(theme[random])
 
