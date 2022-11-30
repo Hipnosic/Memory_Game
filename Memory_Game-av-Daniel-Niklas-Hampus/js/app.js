5997189d230fac7413d802f9d7c82b9bbcbff3e8
ledei <holybuzz@live.se>
2022-11-30 09:26:06 +0100

    Update app.js

--- Memory_Game-av-Daniel-Niklas-Hampus/js/app.js
+++ Memory_Game-av-Daniel-Niklas-Hampus/js/app.js
@@ -1,4 +1,4 @@
-// GATHERING HTML 
+// GATHERING HTML
 let cardContainer = document.querySelector('.card-container')
 let startBtn = document.querySelector('.start-btn')
 let memoryCards = document.querySelectorAll('.card-container')
@@ -34,38 +34,46 @@ let players = [playerOne, playerTwo]
 let gameTurn = 0
 //-----------------------------------------------------------------------
 
-function getPlayesName() { // FETCHING THE PLAYERS USERNAME-INPUT INTO OBJECT
+function getPlayesName() {
+  // FETCHING THE PLAYERS USERNAME-INPUT INTO OBJECT
   let playerOneName = document.querySelector('.playerone-name-field')
   let playerTwoName = document.querySelector('.playertwo-name-field')
   playerOne.name = playerOneName.value
   playerTwo.name = playerTwoName.value
 
+  //HIDES INPUTS FIELD
   playerOneName.classList.add('display-none')
   playerTwoName.classList.add('display-none')
 }
 
-function updateDisplay() { // DISPLAYS THE CURRENT PLAYER AND THEIR SCORE
+function updateDisplay() {
+  // DISPLAYS THE CURRENT PLAYER AND THEIR SCORE
   let currentPlayer = players[gameTurn]
   playerTurnLbl.innerText = currentPlayer.name
   playerOneScorePara.innerText = `${players[0].name}: ${players[0].score}`
   playerTwoScorePara.innerText = `${players[1].name}: ${players[1].score}`
+
+  //SHOWS THE PLAY SITE
   mainContainer.classList.remove('display-none')
 }
 
-function createCard(theme) { // CREATES A TEMPLATE FOR EACH CARD 
+function createCard(theme) {
+  // CREATES A TEMPLATE FOR EACH CARD
   let card = document.createElement('figure')
   card.innerHTML = `<img class ="card-img" src="${theme.name}" alt="${theme.alt}" width = "100">`
   card.className = 'card'
-  card.addEventListener('click', () => { // MAKES THE CARDS CLICKABLE WITH AN ANTI SPAM 
+  card.addEventListener('click', () => {
+    // MAKES THE CARDS CLICKABLE WITH AN ANTI SPAM
     if (!card.classList.contains('flip-front') && !preventClick) {
       gameLogic(card, theme)
     }
   })
 
-  return card 
+  return card
 }
 
-function flipback() { // FLIPS THE CARDS AND THEN FLIPS IT BACK
+function flipback() {
+  // FLIPS THE CARDS AND THEN FLIPS IT BACK
   let rotate = document.querySelectorAll('.flip-front')
   rotate[0].classList.remove('flip-front')
   rotate[0].classList.add('flip-back')
@@ -74,7 +82,8 @@ function flipback() { // FLIPS THE CARDS AND THEN FLIPS IT BACK
   preventClick = false
 }
 
-function matchedCard() { // AFTER THE CARDS MATCHES THEY HIDE THE CARDS
+function matchedCard() {
+  // AFTER THE CARDS MATCHES THEY HIDE THE CARDS
   let matched = document.querySelectorAll('.flip-front')
   matched[0].classList.add('hide')
   matched[1].classList.add('hide')
@@ -86,7 +95,8 @@ function matchedCard() { // AFTER THE CARDS MATCHES THEY HIDE THE CARDS
   winnerChecker()
 }
 
-function restart() {  // RESTARTS THE GAME (DOES NOT RESHUFFLE THE CARDS)
+function restart() {
+  // RESTARTS THE GAME (DOES NOT RESHUFFLE THE CARDS)
   let restartFlip = document.querySelectorAll('.flip-front')
   let restarthidden = document.querySelectorAll('.hide')
   let restartHistory = document.querySelectorAll('.history-para')
@@ -111,12 +121,13 @@ function restart() {  // RESTARTS THE GAME (DOES NOT RESHUFFLE THE CARDS)
   }
 }
 
-function winnerChecker() { // CHECKS IF ALL THE CARDS ARE HIDDEN THEN DISPLAYS THE WINNER OR IF THEY DREW
+function winnerChecker() {
+  // CHECKS IF ALL THE CARDS ARE HIDDEN THEN DISPLAYS THE WINNER OR IF THEY DREW
   let cardAmount = document.querySelectorAll('.hide')
   if (cardAmount[cardCounter]) {
-    mainContainer.style.display = 'none'
-    winnerContainer.style.display = 'flex'
-    winnerContainer.classList.remove('display-none')
+    mainContainer.style.display = 'none' //HIDES THE PLAY SITE
+    winnerContainer.style.display = 'flex' //ADDS FLEX TO WINNER DISPLAY
+    winnerContainer.classList.remove('display-none') //SHOWS THE WINNER DISPLAY
     if (playerOne.score < playerTwo.score) {
       winnerName.innerText = `${playerTwo.name} is the best trainer!`
 
@@ -133,7 +144,8 @@ function winnerChecker() { // CHECKS IF ALL THE CARDS ARE HIDDEN THEN DISPLAYS T
   }
 }
 
-function historyCounter(currentPlayer) {  // DISPLAYS HISTORY OF WHAT POKEMON THE PLAYER CAUGHT!
+function historyCounter(currentPlayer) {
+  // DISPLAYS HISTORY OF WHAT POKEMON THE PLAYER CAUGHT!
   let historyPara = document.createElement('p')
   historyPara.innerText = `${
     currentPlayer.name
@@ -143,7 +155,8 @@ function historyCounter(currentPlayer) {  // DISPLAYS HISTORY OF WHAT POKEMON TH
   historyContainer.append(historyPara)
 }
 
-function gameLogic(card, theme) { // THE RULES OF THE GAME,
+function gameLogic(card, theme) {
+  // THE RULES OF THE GAME,
   card.classList.add('flip-front')
   card.classList.remove('flip-back')
 
@@ -165,7 +178,8 @@ function gameLogic(card, theme) { // THE RULES OF THE GAME,
   setTimeout(updateDisplay, 2000)
 }
 
-function addCardImg(container) { // CREATES EACH CARDS FROM THE THEME-OBJECT. ADDING TO HTML.
+function createDOMCard(container) {
+  // CREATES EACH CARDS WITH A IMG FROM THEME-OBJECT AND USES CREATECARD FUNTCION AS A TAMPLATE. ADDING TO HTML.
   let totalAmount = theme.length
   for (let i = 0; i < totalAmount; i++) {
     let random = Math.floor(Math.random() * theme.length)
@@ -176,20 +190,23 @@ function addCardImg(container) { // CREATES EACH CARDS FROM THE THEME-OBJECT. AD
   }
 }
 
-function initializeCards() {  // PLACES THE CARD ON THE BOARD
-  addCardImg(cardContainer)
+function initializeCards() {
+  // PLACES THE CARD ON THE BOARD
+  createDOMCard(cardContainer)
   getPlayesName()
 }
 //-----------------------------------------------------------------------
-// BUTTON FUNCTIONS 
+// BUTTON FUNCTIONS
 
-startBtn.addEventListener('click', () => { // STARTS THE GAME
+startBtn.addEventListener('click', () => {
+  // STARTS THE GAME
   mainContainer.style.display = 'flex'
   startCard.classList.add('display-none')
   initializeCards()
   updateDisplay()
 })
 
-restartBtn.addEventListener('click', () => { //RESETS THE GAME
+restartBtn.addEventListener('click', () => {
+  //RESETS THE GAME
   restart()
 })
